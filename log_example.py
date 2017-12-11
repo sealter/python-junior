ec91267463043828381db6215417eae23caa5fa1
zchao <hao.sealter@gmail.com>
2017-12-11 15:11:03 +0800

    add book python for finance

--- log_example.py
+++ log_example.py
@@ -14,3 +14,11 @@ logging.critical("logging .... ciritical")
 logger = logging.getLogger("bunyun")
 logger.debug("logger ----- debug")
 logger.info("hello world")
+
+
+
+numeric_level = getattr(logging, loglevel.upper(), None)
+if not isinstance(numeric_level, int):
+    raise ValueError("Invalid log level: %s " % loglevel)
+print("======== numeric_level = ", numeric_level)
+logging.basiConfig(loglevel=numeric_level)
